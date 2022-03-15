b67d802148d4035add871c0ae7882fb1e474a77e
aa380b787 <aa380b787@outlook.com>
2022-03-15 04:27:14 +0200

    main

--- html/index_files/m.js
+++ html/index_files/m.js
@@ -6,5 +6,17 @@ function getScrollPercent() {
     return (h[st]||b[st]) / ((h[sh]||b[sh]) - h.clientHeight) * 360;
 }
 window.onscroll=function(){
-    document.getElementById('logo').style.transform =  "rotate("+getScrollPercent()+"deg)";
+    var s = getScrollPercent();
+    document.getElementById('logo').style.transform =  "rotate("+s+"deg)";
+
+    if(s<40){
+        for (let index = 0; index < document.getElementsByClassName("prj").length; index++) {
+            document.getElementsByClassName("prj")[index].classList.remove("prjj");
+        }
+    }
+    if(s>40){
+        for (let index = 0; index < document.getElementsByClassName("prj").length; index++) {
+            document.getElementsByClassName("prj")[index].classList.add("prjj");
+        }
+    }
 } 
